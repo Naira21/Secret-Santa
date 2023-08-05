52893c0205ed551f00c5777639b7d09cee6ade37
Naira Kazeian <nkazeian@gmail.com>
2023-08-05 21:01:13 +0300

    feat: added entity, main-file, routes

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,52 @@
+import 'reflect-metadata';
 import express, { Request, Response } from 'express';
+import { AppDataSource } from './infrastructure/orm/typeorm/data-source';
+import { User } from './entity/User';
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello mommy!');
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Connected to database');
+  })
+  .catch((error) => console.log('Error connecting to database:', error));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
+
+// app.use(express.json());
+app.get('/users', async (req: Request, res: Response) => {
+  // res.send('Hello mommy!');
+  const users = await AppDataSource.getRepository(User).find();
+  res.json(users);
+});
+
+app.get('/users/:id', async function (req: Request, res: Response) {
+  const results = await AppDataSource.getRepository(User).findOneBy({
+    id: 1,
+  });
+  return res.send(results);
+});
+app.post('/users', async function (req: Request, res: Response) {
+  const user = await AppDataSource.getRepository(User).create({
+    id: 1,
+    name: 'Timber',
+    email: 'Saw',
+  });
+  const results = await AppDataSource.getRepository(User).save(user);
+  return res.send('User saved');
+});
+
+// app.post('/users', async (req, res) => {
+//   const manager = AppDataSource.manager;
+//   const user = manager.create(User, {
+//     id: 1,
+//     name: 'Timber',
+//     email: 'Saw',
+//   });
+
+//   await manager.save(user);
+//   res.send('User saved');
+// });
