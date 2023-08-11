f67df43e2d251ce2c586b3e61be9049d489d1eb5
Naira Kazeian <nkazeian@gmail.com>
2023-08-11 17:00:05 +0300

    added type in userRepo

--- src/infrastructure/repository/userRepository.ts
+++ src/infrastructure/repository/userRepository.ts
@@ -2,9 +2,11 @@ import { DataSource, EntityTarget } from 'typeorm';
 import { User } from '../../domain/user/user';
 
 export class UserRepository {
-  private entity;
+  private entity: EntityTarget<User>;
 
-  constructor(private dbConnection: DataSource) {}
+  constructor(private dbConnection: DataSource) {
+    this.entity = User;
+  }
 
   public saveUser(body: EntityTarget<User>) {
     this.dbConnection.manager.save(body);
