726aaee5fb1fd1711e32d19bcbba158963b88189
Naira Kazeian <nkazeian@gmail.com>
2023-08-22 17:25:01 +0300

    fix: user entity

--- src/domain/user/user.ts
+++ src/domain/user/user.ts
@@ -2,20 +2,48 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
-  constructor(id: number, name: string, email: string) {
-    this.id = id;
-    this.email = email;
-
-    this.name = name;
-  }
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number | null;
 
   @Column({
     length: 50,
+    type: 'varchar',
   })
   name: string;
 
-  @Column()
+  @Column({
+    length: 50,
+    type: 'varchar',
+  })
   email: string;
+
+  constructor(id: number | null, name: string, email: string) {
+    this.id = id;
+    this.email = email;
+    this.name = name;
+  }
+
+  public getId(): number | null {
+    return this.id;
+  }
+
+  public setId(id: number) {
+    this.id = id;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+
+  public setName(name: string) {
+    this.name = name;
+  }
+
+  public getEmail(): string {
+    return this.email;
+  }
+
+  public setEmail(email: string) {
+    this.email = email;
+  }
 }
