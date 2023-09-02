b73f3f84d5fda0602788653844ddbf4b47682c58
Naira Kazeian <nkazeian@gmail.com>
2023-09-02 17:02:37 +0300

    removed extra code

--- src/application/user/useCases/createUserService.ts
+++ src/application/user/useCases/createUserService.ts
@@ -5,14 +5,10 @@ export class CreateUserService {
   constructor(private userRepository: UserRepository) {}
 
   public async createUser(name: string, email: string): Promise<User> {
-    try {
-      const user = new User(null, name, email);
-      console.log(user);
+    const user = new User(null, name, email);
+    console.log(user);
 
-      return await this.userRepository.saveUser(user);
-    } catch (error) {
-      throw error;
-    }
+    return await this.userRepository.saveUser(user);
   }
   // клас окремий коменд квери й прокинути його як боді
   //очікую, що повернеться Юзер
